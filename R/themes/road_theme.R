75a07b7f0e21cf356ba1d4e49543057f33895da6
Matt <moldach@ucalgary.ca>
2019-10-11 22:36:21 -0400

    Add themes

---
+++ R/themes/road_theme.R
@@ -0,0 +1,23 @@
+theme_roads <- function(...) {
+        theme_minimal() +
+                theme(
+                        text = element_text(family = "Hanalei", color = "#5e644f"),
+                        axis.line = element_blank(),
+                        axis.text.x = element_blank(),
+                        axis.text.y = element_blank(),
+                        axis.ticks = element_blank(),
+                        axis.title.x = element_blank(),
+                        axis.title.y = element_blank(),
+                        panel.grid.major = element_blank(),
+                        panel.grid.minor = element_blank(),
+                        plot.background = element_rect(fill = "#f5f5f2", color = NA), 
+                        panel.background = element_rect(fill = "#f5f5f2", color = NA), 
+                        legend.background = element_rect(fill = "#f5f5f2", color = NA),
+                        panel.border = element_blank(),
+                        legend.text = element_text(family = "Hanalei Fill", colour="#A77B56", size=12),
+                        legend.title = element_text(family = "Hanalei Fill", colour="#A77B56"),
+                        legend.position="bottom",
+                        plot.title = element_text(family = "Hanalei", color = "#5e644f", size = 40, hjust = 0.49, vjust = -2),
+                        ...
+                )
+}
