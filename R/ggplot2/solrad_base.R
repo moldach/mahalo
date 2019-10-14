ceaa64136542d8541d0f38d390ef5b82d9527535
Matt <moldach@ucalgary.ca>
2019-10-14 14:52:47 -0400

    Change color palette and alpha

--- R/ggplot2/solrad_base.R
+++ R/ggplot2/solrad_base.R
@@ -19,7 +19,7 @@ make_solrad_map <- function(wetlands_geo = wetlands_geo, solrad_geo = solrad_geo
                         fill = "#012F47",
                         color = "#012F47"
                 ) +
-                geom_sf(data = solrad_geo, aes(fill = solar_cal), color = "white", alpha = 0.3) +
+                geom_sf(data = solrad_geo, aes(fill = solar_cal), color = "white", alpha = 0.7) +
                 scale_fill_manual(values = solrad_colors) +
                 labs(
                         title = paste0("Solar Radiation \n", subtitle),
@@ -29,15 +29,15 @@ make_solrad_map <- function(wetlands_geo = wetlands_geo, solrad_geo = solrad_geo
 }
 
 solrad_colors <- c(
-        "200-250" = "#ffffcc",
-        "250-300" = "#ffeda0",
-        "300-350" = "#fed976",
-        "350-400" = "#feb24c",
-        "400-450" = "#fd8d3c",
-        "450-500" = "#fc4e2a",
-        "500-550" = "#e31a1c",
-        "550-600" = "#bd0026",
-        "600-650" = "#800026")
+        "200-250" = "#FFFCD1",
+        "250-300" = "#FEFF7F",
+        "300-350" = "#FFFF05",
+        "350-400" = "#FFE505",
+        "400-450" = "#FFB805",
+        "450-500" = "#FE612C",
+        "500-550" = "#E04F00",
+        "550-600" = "#D20701",
+        "600-650" = "#3E0001")
 
 solrad_vec <- c( "200-250",
                    "250-300",
